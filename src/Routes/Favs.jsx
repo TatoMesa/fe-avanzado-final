6e870586e6065fc0f09f849fb173eefe824a3ecb
TatoMesa <seba_mesa@hotmail.com>
2023-09-09 16:56:41 -0300

    para acomodar card

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -5,11 +5,12 @@ import {ContextGlobal} from "../Components/utils/global.context.jsx";
 
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
-
+// Actualizar el estado de las tarjetas almacenadas cuando cambie el localStorage
+{/* este componente debe consumir los destacados del localStorage */}
+{/* Deberan renderizar una Card por cada uno de ellos */}
 
 const Favs = () => {
-  const { state } = useContext(ContextGlobal);
-
+  const { state } = useContext(ContextGlobal); //sc-dmBjiZ
   const Container = styled.div`
     background-color: ${state.theme.bgc};
     color: ${state.theme.text};
@@ -17,25 +18,22 @@ const Favs = () => {
 
   const [storedCards, setStoredCards] = useState(JSON.parse(localStorage.getItem('featuredDentist')) || []);
 
-  useEffect(() => {
-    // Actualizar el estado de las tarjetas almacenadas cuando cambie el localStorage
+  {/*useEffect(() => {
     const handleStorageChange = () => {
       setStoredCards(JSON.parse(localStorage.getItem('featuredDentist')) || []);
     };
-
     window.addEventListener("storage", handleStorageChange);
-
     return () => {
       window.removeEventListener("storage", handleStorageChange);
     };
-  }, []);
+  }, []);*/}
+
+  
 
   return (
     <Container>
       <h1>Dentistas Destacados</h1>
       <div className="card-grid">
-        {/* este componente debe consumir los destacados del localStorage */}
-        {/* Deberan renderizar una Card por cada uno de ellos */}
         {storedCards.map((element, id) => {
           return (
             <Card key={id} {...element}>
